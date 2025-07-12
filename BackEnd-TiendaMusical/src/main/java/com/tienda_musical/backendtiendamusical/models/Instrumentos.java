116ae7b909ded088100b8e3feec4096c6045041e
LucasGonzalez <topolucasnicolas@gmail.com>
2025-07-12 16:06:30 -0300

    Convertir BackEnd-TiendaMusical de submódulo a carpeta normal

---
+++ BackEnd-TiendaMusical/src/main/java/com/tienda_musical/backendtiendamusical/models/Instrumentos.java
@@ -0,0 +1,4 @@
+package com.tienda_musical.backendtiendamusical.models;
+
+public class Instrumentos {
+}
---
+++ BackEnd-TiendaMusical/src/test/java/com/tienda_ropa/backendtiendamusical/BackEndTiendaMusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_ropa.backendtiendamusical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTiendaMusicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
